e4cd51b65ec50c07dae702a402d5054e6c94db98
Ahmed <dev@test.com>
2021-06-27 14:12:16 +0300

    Ahmed commit

---
+++ MVC-gg/MVC-gg/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVC_gg.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
